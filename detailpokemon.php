1c38e5d13c401c65f731b68d026f2163eedf397c
Evan <elena.rouzo@proton.me>
2025-03-09 18:40:35 +0100

    Ancetre + Evolution

--- detailpokemon.php
+++ detailpokemon.php
@@ -44,8 +44,18 @@ require_once("head.php");
 
 
 require_once("database-connection.php");
-$sql = "SELECT p.IDPOKEMON, p.NOMPOKEMON, p.URL_PHOTO,p.PV, p.PA, p.PD, p.PVIT, p.PCS, t1.NOMTYPES AS nomtype1 ,t2.NOMTYPES AS nomtype2
+$sql = "SELECT p.IDPOKEMON, p.NOMPOKEMON, p.URL_PHOTO,p.PV, p.PA, p.PD, p.PVIT, p.PCS, t1.NOMTYPES AS nomtype1 ,t2.NOMTYPES AS nomtype2,
+
+e.idEvolution, a.idAncetre
+
+
 FROM pokemon AS p
+
+LEFT JOIN evolutions AS e ON e.idAncetre = p.IDPOKEMON
+LEFT JOIN pokemon AS p2 ON e.idEvolution = p2.IDPOKEMON
+LEFT JOIN evolutions AS a ON a.idEvolution = p.IDPOKEMON
+LEFT JOIN pokemon AS p3 ON a.idAncetre = p3.IDPOKEMON
+
 join typespokemon AS t1 on t1.IDType=p.idType1 
 LEFT join typespokemon AS t2 on t2.IDType=p.idType2
 
@@ -55,6 +65,7 @@ WHERE p.IDPOKEMON =" . $_GET["id"];
 var_dump($_GET);
 
 $result = mysqli_query($databaseConnection,$sql); 
+
 
 if(mysqli_num_rows($result) > 0){
     while($row = mysqli_fetch_assoc($result)){
@@ -80,7 +91,57 @@ if(mysqli_num_rows($result) > 0){
          "<td>" . 
          $row["nomtype2"] . "</td>" .
 
+            "<td>".
+         $row["idAncetre"] . "</td>" .
+         "<td>" . 
+         $row["idEvolution"] . "</td>" .
+
           "</tr>";
+
+        echo 
+
+        "<a href=\"detailpokemon.php?id=" . $row['idAncetre'] . "\">". "Ancetre" ."</a>".
+        "<a href=\"detailpokemon.php?id=" . $row['idEvolution'] . "\">". "Evolution" ."</a>"
+        ;
+    }
+
+}
+
+else{
+    echo "0 results";
+
+}
+
+
+
+
+?>
+
+    </tbody>
+</table>
+
+
+
+    <?php
+    /*
+$sql2 = "SELECT a.idAncetre, e.idEvolution 
+FROM pokemon AS P
+LEFT JOIN evolutions AS e ON e.idAncetre = p.IDPOKEMON
+LEFT JOIN pokemon AS p2 ON e.idEvolution = p2.IDPOKEMON
+LEFT JOIN evolutions AS a ON a.idEvolution = p.IDPOKEMON
+LEFT JOIN pokemon AS p3 ON a.idAncetre = p3.IDPOKEMON
+
+WHERE p.IDPOKEMON =" . $_GET["id"];
+
+$result2 = mysqli_query($databaseConnection,$sql2);
+
+var_dump($result2);
+var_dump($_GET);
+var_dump($row["a.idAncetre"]);
+
+if(mysqli_num_rows($result2) > 0){
+    while($row2 = mysqli_fetch_assoc($result2)){
+        echo $row2["idAncetre"] ;
 
     }
 }
@@ -90,15 +151,8 @@ else{
 
 }
 
- 
-
+*/
 ?>
-
-    </tbody>
-</table>
-
-
-    
 
 
 
